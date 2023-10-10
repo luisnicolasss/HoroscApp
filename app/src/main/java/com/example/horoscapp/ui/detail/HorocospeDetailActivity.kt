959f5f9ea2d858683b3157ff8bb541ac9a59f531
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-10-09 23:59:30 -0300

    create horoscope detail state

--- app/src/main/java/com/example/horoscapp/ui/detail/HorocospeDetailActivity.kt
+++ app/src/main/java/com/example/horoscapp/ui/detail/HorocospeDetailActivity.kt
@@ -3,9 +3,13 @@ package com.example.horoscapp.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.example.horoscapp.databinding.ActivityDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
@@ -18,5 +22,38 @@ class HorocospeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                horoscopeDetailViewModel.state.collect{
+                    when(it){
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Error -> errorState()
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadingState() {
+
+    }
+
+    private fun errorState() {
+
+    }
+
+    private fun successState() {
+
     }
 }

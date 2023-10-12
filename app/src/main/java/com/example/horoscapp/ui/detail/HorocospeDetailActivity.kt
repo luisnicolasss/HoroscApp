6328425ee0a179d368d92b009adae98344a3d8a4
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-10-12 11:56:57 -0300

    Detail Activity with StateFlow, Retrofit, etc and Interceptor on Retrofit

--- app/src/main/java/com/example/horoscapp/ui/detail/HorocospeDetailActivity.kt
+++ app/src/main/java/com/example/horoscapp/ui/detail/HorocospeDetailActivity.kt
@@ -3,11 +3,14 @@ package com.example.horoscapp.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
+import com.example.horoscapp.R
 import com.example.horoscapp.databinding.ActivityDetailBinding
+import com.example.horoscapp.domain.model.HoroscopeModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -23,12 +26,17 @@ class HorocospeDetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initUI()
+        horoscopeDetailViewModel.getHoroscope(args.type)
     }
 
     private fun initUI() {
+        initListeners()
         initUIState()
     }
 
+    private fun initListeners() {
+        binding.ivBack.setOnClickListener { onBackPressed() }
+    }
 
 
     private fun initUIState() {
@@ -38,7 +46,7 @@ class HorocospeDetailActivity : AppCompatActivity() {
                     when(it){
                         HoroscopeDetailState.Loading -> loadingState()
                         is HoroscopeDetailState.Error -> errorState()
-                        is HoroscopeDetailState.Success -> successState()
+                        is HoroscopeDetailState.Success -> successState(it)
                     }
                 }
             }
@@ -46,14 +54,34 @@ class HorocospeDetailActivity : AppCompatActivity() {
     }
 
     private fun loadingState() {
-
+     binding.pb.isVisible = true
     }
 
     private fun errorState() {
-
+        binding.pb.isVisible = false
     }
 
-    private fun successState() {
+    private fun successState(state: HoroscopeDetailState.Success) {
+        binding.pb.isVisible = false
+        binding.tvTitle.text = state.sign
+        binding.tvBody.text = state.prediction
+
+        val image = when(state.horoscopeModel){
+            HoroscopeModel.Aries -> R.drawable.detail_aries
+            HoroscopeModel.Taurus -> R.drawable.detail_taurus
+            HoroscopeModel.Gemini -> R.drawable.detail_gemini
+            HoroscopeModel.Cancer -> R.drawable.detail_cancer
+            HoroscopeModel.Leo -> R.drawable.detail_leo
+            HoroscopeModel.Virgo -> R.drawable.detail_virgo
+            HoroscopeModel.Libra -> R.drawable.detail_libra
+            HoroscopeModel.Scorpio -> R.drawable.detail_scorpio
+            HoroscopeModel.Sagittarius -> R.drawable.detail_sagittarius
+            HoroscopeModel.Capricorn -> R.drawable.detail_capricorn
+            HoroscopeModel.Aquarius -> R.drawable.detail_aquarius
+            HoroscopeModel.Pisces -> R.drawable.detail_pisces
+        }
+
+        binding.ivDetail.setImageResource(image)
 
     }
 }

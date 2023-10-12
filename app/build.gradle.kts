0ed2adadb759b99408c9a65b2c63a631c44fcc18
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-10-12 12:50:25 -0300

    create Interceptor and build types

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,12 +22,23 @@ android {
     }
 
     buildTypes {
-        release {
+
+        getByName("release") {
             isMinifyEnabled = false
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            resValue("string", "arisname", "HoroscApp")
+            buildConfigField("String", "BASE_URL", "\"https://newastro.vercel.app/\"")
+        }
+
+        getByName("debug"){
+            isDebuggable = true
+            resValue("string", "arisname", " [DEBUG] HoroscApp")
+            buildConfigField("String", "BASE_URL", "\"https://newastro-debug.vercel.app/\"")
         }
     }
     compileOptions {
@@ -39,6 +50,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 
 }

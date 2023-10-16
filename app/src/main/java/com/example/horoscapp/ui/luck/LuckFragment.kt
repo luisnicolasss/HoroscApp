898cedd983efe8b398fd50657262be36d4b75f15
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-10-16 12:09:15 -0300

    Spin animation luck fragment

--- app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
@@ -1,21 +1,49 @@
 package com.example.horoscapp.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.FrameMetrics
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
 import com.example.horoscapp.R
 import com.example.horoscapp.databinding.FragmentHoroscopeBinding
 import com.example.horoscapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
 
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.ivRoulette.setOnClickListener { spinRoulette() }
+    }
+
+    private fun spinRoulette() {
+
+        val random = Random()
+        val degrees = random.nextInt(1440) + 360
+
+        val animator = ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION, 0f, degrees.toFloat())
+        animator.duration = 2000
+        animator.interpolator = DecelerateInterpolator()
+        animator.start()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,

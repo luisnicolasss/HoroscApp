eff41327419db971749ff2468d982446ad68d34e
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-10-19 15:39:27 -0300

    Share card luck

--- app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
@@ -1,6 +1,7 @@
 package com.example.horoscapp.ui.luck
 
 import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -40,11 +41,24 @@ class LuckFragment : Fragment() {
     }
 
     private fun preparePrediction() {
-        val luck = randomCardProvider.getLucky()
-        luck?.let {
-            binding.tvLucky.text = getString(it.text)
-            binding.ivLuckyCard.setImageResource(it.image)
+        val currentLuck = randomCardProvider.getLucky()
+        currentLuck?.let {luck ->
+            val currentPrediction = getString(luck.text)
+            binding.tvLucky.text = currentPrediction
+            binding.ivLuckyCard.setImageResource(luck.image)
+            binding.tvShare.setOnClickListener { shareResult(currentPrediction) }
         }
+    }
+
+    private fun shareResult(prediction:String) {
+       val sendIntent = Intent().apply {
+           action = Intent.ACTION_SEND
+           putExtra(Intent.EXTRA_TEXT, prediction)
+           type = "text/plain"
+       }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
     }
 
     private fun initListeners() {

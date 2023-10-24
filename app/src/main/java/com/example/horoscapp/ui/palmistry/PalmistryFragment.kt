33622cf10ec6a192c263dbcc1f005b160fedda63
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-10-24 12:34:04 -0300

    permissions management

--- app/src/main/java/com/example/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.PermissionChecker
 import com.example.horoscapp.R
 import com.example.horoscapp.databinding.FragmentHoroscopeBinding
 import com.example.horoscapp.databinding.FragmentPalmistryBinding
@@ -13,11 +16,38 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
+    companion object {
+        private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
 
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+        isGranted ->
+        if (isGranted){
+          //startCamera
+        }else {
+            Toast.makeText(requireContext(), "Acepta los permisos por favor", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        if (checkCameraPermission()){
+            //Tiene permisos aceptados
+            //startCamera
+        }else {
+           requestPermissionLauncher.launch(CAMERA_PERMISSION)
+        }
+    }
+
+    private fun checkCameraPermission(): Boolean {
+        return PermissionChecker.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.CAMERA
+        ) == PermissionChecker.PERMISSION_GRANTED
     }
 
     override fun onCreateView(

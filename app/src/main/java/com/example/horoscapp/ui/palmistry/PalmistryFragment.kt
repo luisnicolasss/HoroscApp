428fbe8b330793c9adfe7d8ef0fb6f4042a2118f
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-10-24 14:07:52 -0300

    start camera

--- app/src/main/java/com/example/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -1,15 +1,18 @@
 package com.example.horoscapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
-import com.example.horoscapp.R
-import com.example.horoscapp.databinding.FragmentHoroscopeBinding
 import com.example.horoscapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,6 +30,8 @@ class PalmistryFragment : Fragment() {
         isGranted ->
         if (isGranted){
           //startCamera
+            startCamera()
+
         }else {
             Toast.makeText(requireContext(), "Acepta los permisos por favor", Toast.LENGTH_SHORT).show()
         }
@@ -38,9 +43,34 @@ class PalmistryFragment : Fragment() {
         if (checkCameraPermission()){
             //Tiene permisos aceptados
             //startCamera
+            startCamera()
         }else {
            requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
+    }
+
+    private fun startCamera(){
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            }catch (e:Exception){
+                Log.e("luis", "Algo anduvo mal ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     private fun checkCameraPermission(): Boolean {

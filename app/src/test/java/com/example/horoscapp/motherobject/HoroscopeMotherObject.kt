d7c613481a04d84435a70ad562236c511c99a776
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-10-26 16:09:49 -0300

    View model test

--- app/src/test/java/com/example/horoscapp/motherobject/HoroscopeMotherObject.kt
+++ app/src/test/java/com/example/horoscapp/motherobject/HoroscopeMotherObject.kt
@@ -1,9 +1,25 @@
 package com.example.horoscapp.motherobject
 
 import com.example.horoscapp.data.network.response.PredictionResponse
+import com.example.horoscapp.domain.model.HoroscopeInfo
 
 object HoroscopeMotherObject {
 
     val anyResponse = PredictionResponse("date", "prediction", "taurus")
+
+    val horoscopeInfoList = listOf(
+        HoroscopeInfo.Aries,
+        HoroscopeInfo.Taurus,
+        HoroscopeInfo.Geminis,
+        HoroscopeInfo.Cancer,
+        HoroscopeInfo.Leo,
+        HoroscopeInfo.Virgo,
+        HoroscopeInfo.Libra,
+        HoroscopeInfo.Scorpio,
+        HoroscopeInfo.Sagittarius,
+        HoroscopeInfo.Capricorn,
+        HoroscopeInfo.Aquarius,
+        HoroscopeInfo.Pisces
+    )
 
 }
